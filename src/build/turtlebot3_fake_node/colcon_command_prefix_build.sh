5247e478f1708d4aa8822db58f42bcc2ededf380
Katie14242630 <145760969+Katie14242630@users.noreply.github.com>
2024-10-29 22:23:17 +1100

    Delete src directory

--- src/build/turtlebot3_fake_node/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/student/pepper_ws/src/install/turtlebot3_msgs/share/turtlebot3_msgs/package.sh"
--- src/build/turtlebot3_node/colcon_command_prefix_build.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/student/pepper_ws/src/install/dynamixel_sdk/share/dynamixel_sdk/package.sh"
-. "/home/student/pepper_ws/src/install/turtlebot3_msgs/share/turtlebot3_msgs/package.sh"
